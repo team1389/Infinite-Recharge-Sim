5d5799246546e6adfa7895c07ce40b4f40d97233
LittleEggpants <max@breadboard.com>
2020-07-24 14:15:46 -0600

    Added shooting and scoring
    Added shooting and scoring

---
+++ Infinite Recharge Sim/Assets/Scripts/ScorePoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePoints : MonoBehaviour
+{
+    public Text scoreText;
+    int count = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Power Cell")
+        {
+            count += 1;
+            scoreText.text = "Score: " + count;
+        }
+    }
+}
---
+++ Infinite Recharge Sim/Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float shootSpeed;
+    public float angle;
+
+    void FirePowerCell()
+    {
+        Vector3 powerCellStartPosition =  transform.forward.normalized + transform.position;
+
+        Rigidbody powerCellClone = (Rigidbody)Instantiate(projectile, powerCellStartPosition, transform.rotation);
+        powerCellClone.velocity = (transform.forward * (float) Math.Cos((Math.PI / 180) * angle) + (transform.up * (float)Math.Sin((Math.PI / 180) * angle))).normalized * shootSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FirePowerCell();
+        }
+    }
+}
