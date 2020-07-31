3a6b3c240c9c543c333323de32c56238d79046db
Ethan Ma <31868061+ethanma03@users.noreply.github.com>
2020-07-31 14:31:12 -0400

    Fixed lighting and skybox, added field
    Field doesn't have materials and there are still power cells to delete

---
+++ Infinite Recharge Sim/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text scoreText;
+    int count = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Power Cell")
+        {
+            count += 1;
+            scoreText.text = "Score: " + count;
+        }
+    }
+}
---
+++ Infinite Recharge Sim/Assets/Scripts/Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float shootSpeed;
+    public double angle = 60;
+
+    void FirePowerCell()
+    {
+        Vector3 powerCellStartPosition = transform.forward.normalized + transform.position;
+        Rigidbody powerCellClone = (Rigidbody)Instantiate(projectile, powerCellStartPosition, transform.rotation);
+
+        powerCellClone.velocity = ((transform.forward * (float)Math.Cos(angle * (Math.PI/180))) + 
+            (transform.up * (float)Math.Sin(angle * (Math.PI/180)))).normalized * shootSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FirePowerCell();
+        }
+    }
+}
